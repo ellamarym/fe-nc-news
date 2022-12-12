e5845c705238491d18431fac69165ca0f29b8397
ellamarym <ellamuir@icloud.com>
2022-12-12 10:38:13 +0000

    finishes

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -17,16 +17,19 @@ export const LandingPage = () => {
     },[])
     
     return (
-        <section >
-            
-            <h4 className="landing_page_articles">Most popular article</h4>
-        <ul>
+        
+         <ul className="landing_page_articles">   
+            <li>
+            <h4 className="landing_page_headers">Most popular article</h4>
+        
             <ArticleCard key={mostVotesArticle.article_id} author={mostVotesArticle.author} title={mostVotesArticle.title} created_at={mostVotesArticle.created_at} topic={mostVotesArticle.topic} comment_count={mostVotesArticle.comment_count} votes={mostVotesArticle.votes} article_id={mostVotesArticle.article_id} />
-        </ul>
-        <h4 className="landing_page_articles">Most recent article</h4>
-        <ul>
+        </li>
+        <li>
+        <h4 className="landing_page_headers">Most recent article</h4>
+       
         <ArticleCard key={mostRecentArticle.article_id} author={mostRecentArticle.author} title={mostRecentArticle.title} created_at={mostRecentArticle.created_at} topic={mostRecentArticle.topic} comment_count={mostRecentArticle.comment_count} votes={mostRecentArticle.votes} article_id={mostRecentArticle.article_id} />
+        </li>
         </ul>
-        </section>
+        
     )
 }
